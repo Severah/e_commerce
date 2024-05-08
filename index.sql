b8a24769b907990a000b5445f2607516fb522d1f
unknown <severahmwanyumba24@gmail.com>
2024-05-08 14:15:59 +0300

    created user table

--- index.sql
+++ index.sql
@@ -1,1 +1,16 @@
+SHOW DATABASES;
+
+CREATE DATABASE e_commerce;
+
+USE e_commerce;
+
+CREATE TABLE user (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT
+    username VARCHAR(20)
+    email VARCHAR(50)
+    password VARCHAR(10)
+);
+
+INSERT into user VALUES()
+
 SELECT * FROM users;
