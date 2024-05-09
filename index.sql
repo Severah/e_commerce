6cf3aa8acb06695a8044b3b204b5b18cffaf7f14
unknown <severahmwanyumba24@gmail.com>
2024-05-09 16:44:53 +0300

    Added Queries

--- index.sql
+++ index.sql
@@ -5,12 +5,14 @@ CREATE DATABASE e_commerce;
 USE e_commerce;
 
 CREATE TABLE user (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT
-    username VARCHAR(20)
-    email VARCHAR(50)
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(20),
+    email VARCHAR(50),
     password VARCHAR(10)
 );
 
-INSERT into user VALUES()
+SHOW TABLES;
 
-SELECT * FROM users;
+INSERT into user VALUES( 1, "Eugene", "wamalwaeugene@gmail.com", "");
+
+SELECT * FROM  user;
